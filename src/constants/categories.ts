5e6eb89622a99237cbdd0f1b7ff23c30383985d9
Krishna Gupta <kg640620@gmail.com>
2026-06-21 19:59:00 +0530

    Added profile.tsx

---
+++ src/constants/categories.ts
@@ -0,0 +1,14 @@
+export enum ExpenseCategory {
+  FOOD = 'Food',
+  GROCERIES = 'Groceries',
+  TRANSPORT = 'Transport',
+  TRAVEL = 'Travel',
+  ENTERTAINMENT = 'Entertainment',
+  SHOPPING = 'Shopping',
+  BILLS = 'Bills',
+  RENT = 'Rent',
+  HEALTH = 'Health',
+  EDUCATION = 'Education',
+  TRIP = 'Trip',
+  OTHER = 'Other',
+}
